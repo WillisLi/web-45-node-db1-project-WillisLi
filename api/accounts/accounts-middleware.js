5d37cb7c2a77d055218b17ee2b1009d7e27ea859
WillisLi <li.willis95@gmail.com>
2021-09-06 15:56:08 -0700

    completed mvp

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,56 @@
+const Account = require('./accounts-model');
+const db = require("../../data/db-config");
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const error = { status: 400}
+  const account = req.body;
+  if (account.name === undefined || account.budget === undefined) {
+    error.message = "name and budget are required";
+    next(error)
+  } else if (typeof account.name !== "string") {
+    error.message = "name of account must be a string";
+    next(error)
+  } else if (account.name.trim().length < 3 || account.name.trim().length > 100) {
+    error.message = "name of account must be between 3 and 100";
+    next(error)
+  } else if (typeof account.budget !== "number" || isNaN(account.budget)) {
+    error.message = "budget of account must be a number";
+    next(error)
+  } else if (account.budget < 0 || account.budget > 1000000) {
+    error.message = "budget of account is too large or too small";
+    next(error)
+  }
+
+  if (error.message) {
+    next(error)
+  } else {
+    next()
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  const account = req.body;
+  db('accounts').where('name', account.name.trim()).first()
+    .then(exists => {
+      if(exists) {
+        next({ status: 400, message: "that name is taken"})
+      } else {
+        next();
+      }
+    })
+    .catch(next);
 }
 
 exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+  const accountId = req.params.id;
+  Account.getById(accountId)
+    .then(account => {
+      if(account) {
+        req.account = account
+        next()
+      } else {
+        res.status(404).json({ message: "account not found" })
+      }
+    })
+    .catch(next)
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,53 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model');
+const { checkAccountPayload, checkAccountNameUnique, checkAccountId, } = require('./accounts-middleware');
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Accounts.getAll()
+    .then(accountsList => {
+        res.json(accountsList)
+    })
+    .catch(next);
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', checkAccountId, (req, res) => {
+  res.json(req.account);
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
+  const newAccount = {name: req.body.name.trim(), budget: req.body.budget, };
+  Accounts.create(newAccount)
+    .then(insertedAccount => {
+      res.status(201).json(insertedAccount);
+    })
+    .catch(next);
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', checkAccountPayload, checkAccountNameUnique, checkAccountId, (req, res, next) => {
+  const accountId = req.params.id;
+  const updatedInfo = req.body;
+  Accounts.updateById(accountId, updatedInfo)
+    .then(updatedAccount => {
+      res.json(updatedAccount)
+    })
+    .catch(next);
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', checkAccountId, (req, res, next) => {
+  const accountId = req.params.id;
+  Accounts.deleteById(accountId)
+    .then(deletedAccount => {
+      res.json(deletedAccount);
+    })
+    .catch(next);
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    custom: `Something is wrong`,
+    message: err.message,
+    stack: err.stack,
+  }); 
 })
 
 module.exports = router;
